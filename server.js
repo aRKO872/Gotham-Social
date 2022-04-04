f94c224813a25efb2e164fa47429d6e0ad392827
aRKO872 <arkomitra872@gmail.com>
2022-04-04 12:19:05 +0530

    Made a few server side changes to deploy

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path');
 
 const app = express();
 
-app.use(express.json({ extended : false }))
-
 connectDB();
 
-app.get("/", (req, res) => {res.send("API is running")});
+app.use(express.json({ extended : false }))
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 
-app.listen(5000, () => {
-  console.log("Server running at port 5000");
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
 });
